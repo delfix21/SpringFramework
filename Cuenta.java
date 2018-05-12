94ebdcc681eea0f756dd53a0b42cf4c0fecbc2e9
delfix21 <37026974+delfix21@users.noreply.github.com>
2018-05-12 10:12:52 -0500

    Upload eclopseWin10

---
+++ Cuenta.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package net.tecgurus.boot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author root
+ *
+ */
+@Entity
+public class Cuenta {
+	@Id
+	private int idCuenta;
+	private int numeroCuenta;
+	private int idTipoCuenta;
+	private int idCliente;
+	public int getIdCuenta() {
+		return idCuenta;
+	}
+	public void setIdCuenta(int idCuenta) {
+		this.idCuenta = idCuenta;
+	}
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+	public void setNumeroCuenta(int numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+	public int getIdTipoCuenta() {
+		return idTipoCuenta;
+	}
+	public void setIdTipoCuenta(int idTipoCuenta) {
+		this.idTipoCuenta = idTipoCuenta;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	
+}
